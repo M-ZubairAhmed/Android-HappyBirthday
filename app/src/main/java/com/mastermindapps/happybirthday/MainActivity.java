7658d4d9ec52d885061868164bedb01e639cc13d
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-21 19:08:06 +0530

    pun added

--- app/src/main/java/com/mastermindapps/happybirthday/MainActivity.java
+++ app/src/main/java/com/mastermindapps/happybirthday/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mastermindapps.happybirthday;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        finish();
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to quit this amazing app?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"How could you be so heartless?",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                })
+                .create();
+        alertDialog.show();
     }
 }
