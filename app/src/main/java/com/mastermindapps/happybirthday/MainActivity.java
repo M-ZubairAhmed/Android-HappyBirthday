edcb661bdb4b5f040b523b96962db004c80ad7b9
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-22 22:32:46 +0530

    converted all xml to java

--- app/src/main/java/com/mastermindapps/happybirthday/MainActivity.java
+++ app/src/main/java/com/mastermindapps/happybirthday/MainActivity.java
@@ -1,9 +1,13 @@
 package com.mastermindapps.happybirthday;
 
 import android.content.DialogInterface;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawingLayout();
     }
 
     @Override
@@ -34,5 +39,27 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .create();
         alertDialog.show();
+    }
+
+    protected void drawingLayout() {
+        //Root layout
+        android.support.constraint.ConstraintLayout rootLayout = (android.support.constraint.ConstraintLayout) findViewById(R.id.root_layout);
+        rootLayout.setBackgroundResource(R.drawable.androidparty);
+
+        //Text view
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText(R.string.happy_birthday);
+        textView.setTextColor(ContextCompat.getColor(this,R.color.colorAccent));
+        textView.setAllCaps(true);
+        textView.setTypeface(Typeface.DEFAULT,Typeface.BOLD);
+        android.support.constraint.ConstraintLayout.LayoutParams params = new android.support.constraint.ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        int dpForPadding = (int)convertDpPx(28);
+        textView.setPadding(dpForPadding,dpForPadding,dpForPadding,dpForPadding);
+        textView.setTextSize(22);
+    }
+
+    protected float convertDpPx(float dp){
+        float density = getApplicationContext().getResources().getDisplayMetrics().density;
+        return dp * density;
     }
 }
